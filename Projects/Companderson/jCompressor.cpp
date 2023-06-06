3a18f6240f144916cd62512640da15bd2028b743
Jensnln <jens.loeven@student.hku.nl>
2023-06-06 19:47:20 +0200

    -v

---
+++ Projects/Companderson/jCompressor.cpp
@@ -0,0 +1,6 @@
+//
+// Created by Jens on 06/06/2023.
+//
+
+#include "jCompressor.h"
+
---
+++ Projects/Companderson/jCompressor.h
@@ -0,0 +1,14 @@
+//
+// Created by Jens on 06/06/2023.
+//
+
+#pragma once
+#include <juce_audio_processors/juce_audio_processors.h>
+#include <juce_dsp/juce_dsp.h>
+
+
+
+class jCompressor : public juce::dsp::Compressor<float> {
+
+
+};
